41bd2633ba7907bfa139c151e0a8cc03553a1a22
Valentin VERMANDE <vvermande@arketeam.com>
2023-01-12 14:22:47 +0100

    refont stats

--- src/components/PokeDetails.tsx
+++ src/components/PokeDetails.tsx
@@ -1,34 +1,30 @@
-import './PokeDetails.css'
+import './PokeDetails.scss'
 import {useBoxContext} from "../App";
-
-import {TabPanel, TabView} from "primereact/tabview";
 import {PkMoveWithPP} from "../data/PokeTypes";
-import {useState} from "react";
-import {ResponsiveText} from "./ResponsiveText";
-import {NAMES} from "../data/NAMES";
+import {CSSProperties, useState} from "react";
 
 export const PokeDetails = () => {
 
     const {selected_pokemon} = useBoxContext();
+    const [selectedTab, setSelectedTab] = useState(0);
+
     if(!selected_pokemon)
         return <div className="poke-details"></div>;
 
     const species = String(selected_pokemon.pokedex_id).padStart(3, '0'); // 7 -> '007'
 
-    const name = `${selected_pokemon.nickname} (${NAMES[selected_pokemon.pokedex_id]})`
+    //const name = `${selected_pokemon.nickname} (${NAMES[selected_pokemon.pokedex_id]})`
 
     const stats = [
-        [name, 'Lvl '+selected_pokemon.level],
-        ['OT', selected_pokemon.OT_name],
-        ['XP', selected_pokemon.exp],
-        ['HP', `${selected_pokemon.current_hp}/${selected_pokemon.stats.hp}`],
-        ['ATK', selected_pokemon.stats.atk],
-        ['DEF', selected_pokemon.stats.def],
-        ['SPE', selected_pokemon.stats.atk_spe],
-        ['SPD', selected_pokemon.stats.spd]
+        'HP', `${selected_pokemon.current_hp}/${selected_pokemon.stats.hp}`,
+        'ATK', selected_pokemon.stats.atk,
+        'DEF', selected_pokemon.stats.def,
+        'SP.ATK', selected_pokemon.stats.atk_spe,
+        'SP.DEF', selected_pokemon.stats.atk_spe,
+        'SPD', selected_pokemon.stats.spd
     ];
 
-    console.log(selected_pokemon)
+    const xp_data = ['EXP_POINTS', selected_pokemon.exp, 'NEXT LV.', 'N/A'];
 
     return <div className="poke-details">
         <img alt="" src={`${process.env.PUBLIC_URL}/icons/${species}.png`} className="poke-img cursor-pointer"/>
@@ -36,45 +32,51 @@ export const PokeDetails = () => {
             {selected_pokemon.types.map(type =>
                 <img alt="" className="type-badge" src={process.env.PUBLIC_URL + '/types/' + type.toLowerCase() + '.png'}/>)}
         </div>
-        <div className="poke-data">
-            <TabView>
-                <TabPanel headerTemplate={({onClick}) => <img alt="" onClick={onClick} className="header-icon cursor-pointer" src={`${process.env.PUBLIC_URL}/imgs/stats.svg`}></img>}>
-                    <div className="poke-stats poke-font">
-                        {stats.map((s, i) => <div key={i} className="flex justify-content-between">
-                            <span>{s[0]}</span>
-                            <span>{s[1]}</span>
-                        </div>)}
-                    </div>
-                </TabPanel>
-                <TabPanel headerTemplate={({onClick}) => <img alt="" onClick={onClick} className="header-icon cursor-pointer" src={`${process.env.PUBLIC_URL}/imgs/sword.svg`}></img>}>
-                    <MovesInfo moves={selected_pokemon.moves}></MovesInfo>
-                </TabPanel>
-            </TabView>
+        <div className={`tab-select tab-${selectedTab} flex justify-content-around align-items-center`}>
+            {[0, 1, 2, 3].map(i =>
+                <div key={i} onClick={() => setSelectedTab(i)}></div>
+            )}
         </div>
+
+        {selectedTab === 0 && <div className="tab-content stats-tab">
+            <div className="stats">
+                {stats.map((stat, i) => <span key={i} className="poke-font">{stat}</span>)}
+            </div>
+            <div className="xp">
+                {xp_data.map((stat, i) => <span key={i} className="poke-font">{stat}</span>)}
+            </div>
+        </div>}
+
+        {selectedTab === 1 && <div className="tab-content moves-tab">
+            <MovesInfo moves={selected_pokemon.moves}></MovesInfo>
+        </div>}
     </div>
 }
 
 function MovesInfo(props: { moves: PkMoveWithPP[] }) {
 
-    const [selectedMoveIndex, setSelectedMoveIndex] = useState(-1);
+    const [selectedMoveIndex, setSelectedMoveIndex] = useState(0);
     const selected_move = props.moves[selectedMoveIndex];
 
-    return <div className="grid">
-        {props.moves.map((move, i) =>
-            <div className={'col-12 cursor-pointer grid poke-font ' + (i === selectedMoveIndex ? 'selected' : '')} key={i} onClick={() => setSelectedMoveIndex(i)}>
-                <img alt="" className="type-badge col-2" src={process.env.PUBLIC_URL + '/types/' + move.type.toLowerCase() + '.png'}/>
-                <div className="col"><ResponsiveText text={move.name}/></div>
-                <div className="col-2">{move.actual_PP}/{move.PP}PP</div>
-            </div>
-        )}
+    return <>
+        <div className="moves poke-font">
+            {props.moves.map((move, i) => <>
+                <img onClick={() => setSelectedMoveIndex(i)} alt="" className="type-badge cursor-pointer" src={process.env.PUBLIC_URL + '/types/' + move.type.toLowerCase() + '.png'}/>
+                <span onClick={() => setSelectedMoveIndex(i)}>{move.name.toUpperCase()}</span>
+                <span onClick={() => setSelectedMoveIndex(i)}> <small>PP</small> {move.actual_PP}/{move.PP}</span>
+            </>)}
+        </div>
 
-        {selected_move && <div className="poke-font col-12">
-            {selected_move.effect && selected_move.effect.length && <p className="overflow-y-auto max-h-10rem">{selected_move.effect}</p>}
-            <div className="grid">
-                <div className="col">POWER {selected_move.power}</div>
-                <div className="col">ACCURACY {selected_move.accuracy}</div>
-            </div>
+        <img className="move-selection"
+             src={process.env.PUBLIC_URL + '/imgs/move_selection.png'}
+             style={{'--i': selectedMoveIndex} as CSSProperties}
+             alt=""
+        />
+
+        {selected_move && <div className="poke-font description">
+            {selected_move.effect && selected_move.effect.length && <span className="overflow-y-auto">{selected_move.effect}</span>}
         </div>}
-    </div>
+
+    </>
 }
 
